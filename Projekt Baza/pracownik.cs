fd6c4f49abe2aa4fbf9e12fd9380450babb79eee
Szymon Poznański <128556@student.uni.opole.pl>
2020-11-21 11:51:42 +0100

    class pracownik.cs

---
+++ Projekt Baza/pracownik.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Baza
+{
+    class pracownik
+    {
+        public int idpracownika { get; set; }
+        public string nazwisko { get; set; }
+        public string imie { get; set; }
+        public int pesel { get; set; }
+        public string stanowisko { get; set; }
+        public int wiek { get; set; }
+    }
+}
